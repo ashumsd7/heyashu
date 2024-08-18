292c0e2c481e153ff4b405e77dcda6aa1f2d4672
Ashutosh T <ashutosh.tiwari@codemonk.in>
2024-08-18 22:39:12 +0530

    launch episode 0

--- src/data/tech/notes/markdown/namaste_node_js_by_as_s1/e0.js
+++ src/data/tech/notes/markdown/namaste_node_js_by_as_s1/e0.js
@@ -1,17 +1,41 @@
 export const  e0=`
+# E0: Welcome to Namaste Node JS
 
-# About the Creator
+![image.png](https://i.ibb.co/2hq8tjW/akshays-banner.jpg)
 
-Akshay  started building websites in the 8th grade.
+# About the Creator (The Boss, Akshay Saini)
 
-He has worked at Paytm, Uber, and multiple startups.
+Akshay started building websites in the 7th or 8th grade.
 
-## Requests from the Creator Before Starting the Course
+He has worked at Paytm, Uber, [Lendingkart](https://www.lendingkart.com/business-loan/check-eligibility), and multiple startups, where he learned how to develop large-scale companies.
+
+He started a startup while he was still in college.
+
+Multiple things have helped him in his career; one of those is  \`Being Curious\`.
+
+Having curiosity and the desire to learn new things will help you become better and better.
+
+Ask questions! This will help you in your career.
+
+He encourages you to be curious during this course as well.
+
+# Requests from the Creator Before Starting the Course
 
 1. Keep taking notes, either digitally or in a notebook, as you follow along with the videos.
-2. Practice the concepts along with the video.
-3. You can write notes in Notion, Evernote, or on your personal website. *(This is a suggestion, not from the creator)*
-4. Focus during the video. Silence your phones.
-5. Plan your course and stick to it.
+2. Practice the concepts as you watch the videos.
+3. You can write notes in Notion, Evernote, or on your personal website (this suggestion is from me, not the creator).
+4. Focus during the videos. Silence your phones.
+5. Plan and schedule your course, and stick to it.
+6. Watch one episode, then take a break. Try to explore the concepts on the internet, implement them on your system, and then move on to the next one.
+7. Join the Discord community (the link is available within the course).
+8. Ask questions and help each other out.
+9. Make friends.
+10. Refer each other in your respective careers.
+11. Give your full attention, focus, and practice to this course.
+
+Okay… Let’s get started…
+
+Here we go 😄
+
 
 `
