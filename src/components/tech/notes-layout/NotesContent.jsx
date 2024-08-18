e0a2141e385ae2351b27de1fbea2fc7bc3580572
Ashutosh T <ashutosh.tiwari@codemonk.in>
2024-08-18 15:05:29 +0530

    update : Markdown

--- src/components/tech/notes-layout/NotesContent.jsx
+++ src/components/tech/notes-layout/NotesContent.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 function NotesContent({markdownContent}) {
   return (
     <div className='p-4'>
-        <ReactMarkdown className="prose xl:prose-xl lg:prose-lg md:prose-md sm:prose-sm">{markdownContent}</ReactMarkdown>
+        <ReactMarkdown className="prose prose-xl lg:prose-lg md:prose-md sm:prose-sm">{markdownContent}</ReactMarkdown>
     </div>
   )
 }
