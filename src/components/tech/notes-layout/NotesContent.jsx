38f865e71f965e463f84f30840531c1f35ab43c7
Ashutosh T <ashutosh.tiwari@codemonk.in>
2024-08-18 15:12:45 +0530

    update : Markdown

--- src/components/tech/notes-layout/NotesContent.jsx
+++ src/components/tech/notes-layout/NotesContent.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown';
-function NotesContent({markdownContent}) {
+import React from "react";
+import ReactMarkdown from "react-markdown";
+function NotesContent({ markdownContent }) {
   return (
-    <div className='p-4'>
-        <ReactMarkdown className="prose prose-xl lg:prose-lg md:prose-md sm:prose-sm">{markdownContent}</ReactMarkdown>
+    <div className="p-4">
+      <ReactMarkdown className="prose xl:prose-xl lg:prose-lg md:prose-md sm:prose-sm">
+        {markdownContent}
+      </ReactMarkdown>
     </div>
-  )
+  );
 }
 
-export default NotesContent
+export default NotesContent;
