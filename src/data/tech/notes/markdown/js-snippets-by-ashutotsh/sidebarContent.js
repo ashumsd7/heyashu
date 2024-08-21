8a188c7f127dff6084ed1b953cd7527207b7c512
Ashutosh T <ashutosh.tiwari@codemonk.in>
2024-08-21 18:00:04 +0530

    update : add date

--- src/data/tech/notes/markdown/js-snippets-by-ashutotsh/sidebarContent.js
+++ src/data/tech/notes/markdown/js-snippets-by-ashutotsh/sidebarContent.js
@@ -1,5 +1,5 @@
 export const JS_SNIPPETS_BY_ASHUTOSH = [
-  { id: 1, episode: 1, name: "Data Types" },
+  { id: 1, episode: 1, name: "Data Types", publishedOn: '21 Aug 2024' },
   { id: 2, episode: 2, name: "Function Declaration & Expressions" },
   { id: 3, episode: 3, name: "Destructuring", publishedOn: '18 Aug 2024' },
   { id: 4, episode: 4, name: "Spread Operator" },
