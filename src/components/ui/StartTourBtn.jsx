d346c569b411cb29a7e59347865725dda8d68120
Ashutosh T <aat8001@gmail.com>
2024-08-25 10:24:11 +0530

    fix: hide tour button on mobiel screen

--- src/components/ui/StartTourBtn.jsx
+++ src/components/ui/StartTourBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { MdOutlineTour } from "react-icons/md";
 function StartTour({children='',onClick}) {
   return (
-    <button  id='start-tour'  title='Start the Website Tour again' onClick={onClick} className="border-b-32 border-dotted font-serif flex items-center gap-1 font-semibold cursor-pointer bg-gray-800  text-gray-100 px-2 py-1 rounded-full h-10 w-10  bottom-2 justify-center"><MdOutlineTour />{children}</button>
+    <button  id='start-tour'  title='Start the Website Tour again' onClick={onClick} className="border-b-32 hidden border-dotted font-serif lg:flex items-center gap-1 font-semibold cursor-pointer bg-gray-800  text-gray-100 px-2 py-1 rounded-full h-10 w-10  bottom-2 justify-center"><MdOutlineTour />{children}</button>
   )
 }
 
