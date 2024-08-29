bed23ec37c47d6057f479c972c34e112a7e3f2ad
Ashutosh T <aat8001@gmail.com>
2024-08-29 22:10:43 +0530

    add: Remove  me a coffee btn

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -101,11 +101,11 @@ export default function App({ Component, pageProps }) {
               window.open(PHONE_CALL_THIRTY_MIN, "_blank");
             }}
           />
-          <a href="https://www.buymeacoffee.com/ashumsd7" style={{
+          {/* <a href="https://www.buymeacoffee.com/ashumsd7" style={{
             fontSize:'20px'
           }}>
             <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a laptop&emoji=💻️&slug=ashumsd7&button_colour=FF5F5F&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" />
-          </a>
+          </a> */}
         </div>
       </main>
     </>
