e9dac033604478bdc32bea6aff0fdc954d96883f
Ashutosh T <aat8001@gmail.com>
2024-09-01 02:35:51 +0530

    fix: import issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 import StartTour from "@/components/ui/StartTourBtn";
 import { useEffect } from "react";
+import dynamic from "next/dynamic";
 
 const CMS = dynamic(
   () =>
