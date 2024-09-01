8dcbbec2a4ac901a4ecb5de5dcd1849f4c1dad5d
Ashutosh T <aat8001@gmail.com>
2024-09-01 18:33:22 +0530

    feat: all blog page

---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+function MainBlogsPage({ frontMatter, mdxSource, large = false }) {
+  return (
+    <div>All blogs page</div>
+  )
+}
+
+export default MainBlogsPage
+
