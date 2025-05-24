4ed8ebcddd841a9ef2edef81f285c92f73187711
Aashutosh Anand Tiwari <40313523+ashumsd7@users.noreply.github.com>
2025-05-25 02:40:35 +0530

    Update MainPage.jsx
    Signed-off-by: Aashutosh Anand Tiwari <40313523+ashumsd7@users.noreply.github.com>

--- src/components/Home/MainPage.jsx
+++ src/components/Home/MainPage.jsx
@@ -106,7 +106,7 @@ function MainPage() {
     >
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16 max-w-7xl">
         {/* Live Event Banner */}
-        <LiveEventBanner />
+        
         
         {/* Hero Section */}
         <motion.div
